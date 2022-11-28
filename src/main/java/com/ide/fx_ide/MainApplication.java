549bff47461403456ae0b92391d273d26321f5d5
reidlo5135 <201840103@email.daelim.ac.kr>
2022-11-28 17:29:00 +0900

    feat : add unversioned files

--- src/main/java/com/ide/fx_ide/MainApplication.java
+++ src/main/java/com/ide/fx_ide/MainApplication.java
@@ -3,25 +3,31 @@ package com.ide.fx_ide;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
     private static final String DIRECTORY_CSS = "static/css/";
+    private static final String DIRECTORY_IMAGE = "static/image/";
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("root.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1680, 840);
-        scene.getStylesheets().add(getClass().getResource(DIRECTORY_CSS + "root.css").toString());
-        stage.setTitle("RDE");
         stage.setScene(scene);
         stage.setResizable(false);
+        setResources(stage, scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private static void setResources(Stage stage, Scene scene) {
+        scene.getStylesheets().add(MainApplication.class.getResource(DIRECTORY_CSS + "root.css").toString());
+        stage.getIcons().add(new Image(MainApplication.class.getResource(DIRECTORY_IMAGE + "favicon.png").toString()));
     }
 }
--- src/main/java/com/ide/fx_ide/root/RootController.java
+++ src/main/java/com/ide/fx_ide/root/RootController.java
@@ -2,16 +2,28 @@ package com.ide.fx_ide.root;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RootController implements Initializable {
+    @FXML private MenuBar mb_top;
     @FXML private TextArea ta_code;
+
+    private static final String[] mb_top_arr = {"File", "Edit", "View", "Navigate", "Code", "Refactor", "Build", "Run", "Tools", "Git", "Window", "Help"};
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        setMb_top_arr(mb_top);
         ta_code.setText("class Main {\n\tpublic static void main(String[] args) {\n\n\t}\n}");
+    }
+
+    private static void setMb_top_arr(MenuBar menuBar) {
+        for(String str : mb_top_arr) {
+            menuBar.getMenus().add(new Menu(str));
+        }
     }
 }
