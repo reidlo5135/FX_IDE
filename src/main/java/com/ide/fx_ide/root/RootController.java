2071c78d13010fcffd3ee2a152faa63e4b0d7f4a
reidlo5135 <201840103@email.daelim.ac.kr>
2022-12-03 12:22:17 +0900

    feat : add Compile Logics

--- src/main/java/com/ide/fx_ide/root/RootController.java
+++ src/main/java/com/ide/fx_ide/root/RootController.java
@@ -1,7 +1,9 @@
 package com.ide.fx_ide.root;
 
+import com.ide.fx_ide.compiler.CompilerService;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextArea;
@@ -12,9 +14,11 @@ import java.util.ResourceBundle;
 public class RootController implements Initializable {
     @FXML private MenuBar mb_top;
     @FXML private TextArea ta_code;
+    @FXML private Button btn_compile;
 
     private static final String DEFAULT_CODE = "class Main {\n\tpublic static void main(String[] args) {\n\n\t}\n}";
     private static final String[] DEFAULT_TOP_MENU = {"File", "Edit", "View", "Navigate", "Code", "Refactor", "Build", "Run", "Tools", "Git", "Window", "Help"};
+    private final CompilerService compilerService = new CompilerService();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -26,5 +30,12 @@ public class RootController implements Initializable {
         for(String str : DEFAULT_TOP_MENU) {
             menuBar.getMenus().add(new Menu(str));
         }
+    }
+
+    @FXML
+    protected void onCompileButtonClick() {
+        String beforeCompile = ta_code.getText();
+        System.out.println("beforeCompile : " + beforeCompile);
+        System.out.println(compilerService.compileAfterConvertFile(beforeCompile).get(1));
     }
 }
