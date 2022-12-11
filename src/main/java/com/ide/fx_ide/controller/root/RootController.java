4ac3cd133e5e5118f1d2b45c74c3008fe46e3775
reidlo5135 <201840103@email.daelim.ac.kr>
2022-12-11 10:49:05 +0900

    refactor : modify moveToEditScene Logic

--- src/main/java/com/ide/fx_ide/controller/root/RootController.java
+++ src/main/java/com/ide/fx_ide/controller/root/RootController.java
@@ -1,20 +1,14 @@
 package com.ide.fx_ide.controller.root;
 
-import com.ide.fx_ide.MainApplication;
 import com.ide.fx_ide.service.common.CommonService;
 import com.ide.fx_ide.service.file.FileService;
-import com.ide.fx_ide.controller.edit.EditorController;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 public class RootController implements Initializable {
@@ -34,29 +28,7 @@ public class RootController implements Initializable {
 
     @FXML
     protected void onSelectButtonClick() {
-        Map<String, String> map = fileService.setFileChooser(stage);
-        Stage stage_file = (Stage) btn_select.getScene().getWindow();
-        String title = map.get("path") + " - " + map.get("name");
-
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApplication.class.getResource("editor.fxml"));
-            Parent root = loader.load();
-            Scene scene = new Scene(root, 1680, 840);
-            EditorController editorController = loader.getController();
-            editorController.setTextArea(map.get("code"));
-
-            Stage stage_editor = new Stage();
-            stage_editor.setScene(scene);
-            stage_editor.setTitle(title);
-            CommonService.setResources(stage_editor, scene, "editor.css", "favicon.png");
-            stage_editor.show();
-
-            stage_file.close();
-            stage_file.hide();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        CommonService.moveToEditScene(fileService.setFileChooser(stage), (Stage) btn_select.getScene().getWindow());
     }
 
     @FXML
--- src/main/java/com/ide/fx_ide/service/common/CommonService.java
+++ src/main/java/com/ide/fx_ide/service/common/CommonService.java
@@ -1,10 +1,14 @@
 package com.ide.fx_ide.service.common;
 
 import com.ide.fx_ide.MainApplication;
+import com.ide.fx_ide.controller.edit.EditorController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.Map;
 import java.util.Objects;
 
 public class CommonService {
@@ -14,5 +18,29 @@ public class CommonService {
     public static void setResources(Stage stage, Scene scene, String css, String favicon) {
         scene.getStylesheets().add(Objects.requireNonNull(MainApplication.class.getResource(DIRECTORY_CSS + css)).toString());
         stage.getIcons().add(new Image(Objects.requireNonNull(MainApplication.class.getResource(DIRECTORY_IMAGE + favicon)).toString()));
+    }
+
+    public static void moveToEditScene(Map<String, String> data, Stage stage_previous) {
+        String title = data.get("path") + " - " + data.get("name");
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApplication.class.getResource("editor.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1680, 840);
+            EditorController editorController = loader.getController();
+            editorController.setTextArea(data.get("code"));
+
+            Stage stage_editor = new Stage();
+            stage_editor.setScene(scene);
+            stage_editor.setTitle(title);
+            CommonService.setResources(stage_editor, scene, "editor.css", "favicon.png");
+            stage_editor.show();
+
+            stage_previous.close();
+            stage_previous.hide();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
